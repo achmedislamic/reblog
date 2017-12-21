aa98856e28298b52802d52d37c9eb98a3acd35f5
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2017-12-21 08:59:37 +0700

    reblog from start done

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+<form method="POST" action="/posts">
+{{ csrf_field() }}
+  <div class="form-group">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" name="title">
+  </div>
+  <div class="form-group">
+    <label for="body">Body</label>
+    <textarea name="body" class="form-control"></textarea>
+  </div>
+  <div class="form-group">
+    <button type="submit" class="btn btn-primary">Publish</button>
+  
+  </div>
+</form>
+@include('layouts.errors')
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+@foreach($posts as $post)
+<div class="blog-post">
+            <h2 class="blog-post-title"><a href="/posts/{{$post->id}}">{{$post->title}}</a></h2>
+            <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}}</p>
+            {{$post->body}}
+            
+          </div><!-- /.blog-post -->
+@endforeach
+
+@endsection
